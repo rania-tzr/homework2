083809fb512fe63aa0ad5b461b63451fb27b49b5
TAZIR Rania <85077665+rania-tzr@users.noreply.github.com>
2022-03-23 22:23:48 +0100

    Add files via upload

---
+++ HomeWork2.js
@@ -0,0 +1,25 @@
+var person1 = {
+    name: "Rania",
+    discord_id:"Rania#5482",
+        
+    }
+  
+  
+var person2 = {
+    name: "Dounia",
+    discord_id:"Dounia#5582"
+   
+}
+ 
+function compare(person1,person2){
+    
+  for(var i in person1){
+     if(person1[i]==person2[i]){
+         return true;
+      }
+      
+}
+return false;
+}
+
+console.log(compare(person1,person2));
